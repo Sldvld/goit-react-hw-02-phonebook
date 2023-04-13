c64a314cee1ac7e88c686954ec9f073c8528e22f
Sldvlld <sladvlad@gmail.com>
2023-04-13 17:44:55 +0300

    add Form Component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { nanoid } from 'nanoid';
+import { Form } from './Form/Form';
+export class App extends React.Component {
+  state = {
+    contacts: [],
+  };
+
+  formSubmitHandler = data => {
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div>
+        <Form onSubmit={this.formSubmitHandler} />
+      </div>
+    );
+    // return (
+    //   <form onSubmit={this.handleSubmit}>
+    //     <label htmlFor="name">Name</label>
+    //     <input
+    //       type="text"
+    //       name="name"
+    //       value={this.state.name}
+    //       onChange={this.handleChange}
+    //       pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+    //       title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+    //       required
+    //     />
+    //     <label htmlFor="number">Number</label>
+    //     <input
+    //       type="tel"
+    //       name="number"
+    //       value={this.state.number}
+    //       onChange={this.handleChange}
+    //       pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+    //       title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+    //       required
+    //     />
+    //     <button type="submit">Add contact</button>
+    //   </form>
+    // );
+  }
+}
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+export class Form extends React.Component {
+  state = {
+    name: '',
+    number: '',
+  };
+  handleChange = evt => {
+    const { name, value } = evt.currentTarget;
+    this.setState({ [name]: value });
+  };
+  handleSubmit = evt => {
+    evt.preventDefault();
+    this.props.onSubmit(this.state);
+    this.resetForm();
+  };
+  resetForm = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          value={this.state.name}
+          onChange={this.handleChange}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+        <label htmlFor="number">Number</label>
+        <input
+          type="tel"
+          name="number"
+          value={this.state.number}
+          onChange={this.handleChange}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
+}
